46b561af251cfda9c1c497f9dd4d79341e8595a2
chigozienri <chigozie@gmail.com>
2023-11-01 15:20:02 +0000

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "replicate.com",
+      },
+      {
+        protocol: "https",
+        hostname: "replicate.delivery",
+      },
+      {
+        protocol: "https",
+        hostname: "*.replicate.delivery",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
