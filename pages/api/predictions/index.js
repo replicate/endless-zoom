68d5321e6bb05fe5401fbd0f1e1dd970e7a1d3dc
Chigozie Nri <chigozie@gmail.com>
2023-11-08 12:15:32 +0000

    Send num_inference_steps to replicate

--- pages/api/predictions/index.js
+++ pages/api/predictions/index.js
@@ -10,13 +10,14 @@ export default async function handler(req, res) {
       "The REPLICATE_API_TOKEN environment variable is not set. See README.md for instructions on how to set it."
     );
   }
-
+  // local uses "steps", while replicate uses "num_inference_steps"
+  let input = req.body.input;
+  input.num_inference_steps = input.steps;
   var prediction = await replicate.run(
     'fofr/latent-consistency-model:92b456763bbd035ef9dda7cc778c9cc8628cb4b48b8af813d541e78565342330',
     {
-      input: req.body.input,
+      input: input,
     });
-  console.log(req.body.input);
 
   if (prediction?.error) {
     res.statusCode = 500;
