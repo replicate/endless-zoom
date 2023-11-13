51175ae3ead103deeffe6addd22299b87131bb35
Chigozie Nri <chigozie@gmail.com>
2023-11-13 21:17:22 +0000

    Remove top banner

--- pages/index.js
+++ pages/index.js
@@ -13,17 +13,6 @@ export default function Home() {
         <meta property="twitter:image" content={`${rootUrl}/og-image.png`} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
-      <div className="bg-slate-100 border-b-2 text-center p-3">
-        Powered by Replicate.{" "}
-        <a
-          href="https://replicate.com/fofr/latent-consistency-model?utm_source=project&utm_campaign=endless-zoom"
-          target="_blank"
-          rel="noreferrer"
-          className="underline"
-        >
-          Run this model in the cloud
-        </a>
-      </div>
       <div id="content"></div>
       <Script src="https://cdn.jsdelivr.net/npm/p5@1.8.0/lib/p5.js" />
       <Script src="./script.js" strategy="beforeInteractive" />
